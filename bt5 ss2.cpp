edb12cefadba585c7f47f45e006105680acc2532
ThanhDat-hub <daithanhdat73@gmail.com>
2024-12-10 23:29:07 +0700

    Add files via upload

---
+++ bt5 ss2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int lenght = 5, width = 10;
+    int perimeter = (lenght + width) * 2, area = lenght * width;
+    printf("chu vi = %d\n", perimeter);
+    printf("dien tich = %d", area);
+    
+    return 0;
+}
